22c95077b1a6132a3fde25ad2a93eae8acf85e9f
davoodinator <135515620+McVaxius@users.noreply.github.com>
2026-03-13 00:53:18 -0400

    ✅ Successfully bumped VERMAXION to version 0.0.0.32

--- VERMAXION/Services/FCBuffService.cs
+++ VERMAXION/Services/FCBuffService.cs
@@ -255,7 +255,7 @@ public class FCBuffService : IDisposable
         return gcTerritory switch
         {
             129 => new Vector3(94, 40f, 68f),     // Limsa (Upper Decks/Aft)
-            132 => new Vector3(-72.8, -0.5f, -5.7f),   // Gridania
+            132 => new Vector3(-72.8f, -0.5f, -5.7f),   // Gridania
             130 => new Vector3(-145, 4f, -101f),   // Ul'dah
             _ => Vector3.Zero
         };
